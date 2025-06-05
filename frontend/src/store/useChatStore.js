751795b431bd82f9becb19ce8fa66ce3f0b7245e
pawan sharma <102892764+pawanshama@users.noreply.github.com>
2025-06-05 21:43:12 +0530

    Update useChatStore.js

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -9,6 +9,7 @@ export const useChatStore = create((set, get) => ({
   selectedUser: null,
   isUsersLoading: false,
   isMessagesLoading: false,
+  newUser:[]
 
   getUsers: async () => {
     set({ isUsersLoading: true });
